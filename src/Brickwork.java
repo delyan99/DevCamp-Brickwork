76c743c6766f02cafe3b790fedf05b88fe2bce83
Delyan Milchev <d_milchev99@abv.bg>
2020-12-24 23:33:36 +0200

    Add fixes for printing the second layer

--- src/Brickwork.java
+++ src/Brickwork.java
@@ -9,9 +9,9 @@ public class Brickwork {
         String[] areaOfSize = scanner.nextLine().split("\\s+");
 
         //check if input is valid
-        if(!isWidthOrLengthValid(areaOfSize)){
+        if (!isWidthOrLengthValid(areaOfSize)) {
             return;
-        };
+        }
 
         //take width and length ot all bricks
         int width = Integer.parseInt(areaOfSize[0]);
@@ -26,7 +26,7 @@ public class Brickwork {
         int[][] secondLayer = new int[width][length];
 
         //fill first layer
-        if(addToFirstLayer(scanner, firstLayer, length)){
+        if (addToFirstLayer(scanner, firstLayer, length)) {
             return;
         }
 
@@ -38,14 +38,14 @@ public class Brickwork {
     }
 
     private static boolean isWidthOrLengthValid(String[] areaOfSize) {
-        if(areaOfSize.length == 2) {
+        if (areaOfSize.length == 2) {
             try {
                 int width = Integer.parseInt(areaOfSize[0]);
                 int length = Integer.parseInt(areaOfSize[1]);
                 if (width % 2 == 0 && length % 2 == 0 && width < 100 && length < 100 && width > 1 && length > 1) {
                     return true;
                 }
-            }catch (NumberFormatException e){
+            } catch (NumberFormatException e) {
                 System.out.println("-1");
                 System.out.println("No solution exists.");
                 return false;
@@ -70,7 +70,6 @@ public class Brickwork {
                     j++;
                 }
             }
-
             if (secondLayer[i][firstLayer[i].length - 1] == 0) {
                 secondLayer[i][firstLayer[i].length - 1] = brickNumbers.peek();
                 secondLayer[i + 1][firstLayer[i].length - 1] = brickNumbers.pop();
@@ -86,131 +85,72 @@ public class Brickwork {
 
     private static void printSecondLayer(int[][] secondLayer) {
         for (int i = 0; i < secondLayer.length; ) {
-            if(i == 0) {
+            if (i == 0) {
                 for (int j = 0; j < secondLayer[i + 1].length; j++) {
-                    int currentNUmber = secondLayer[i + 1][j];
                     if (j == 0) {
                         System.out.print("   ");
                     }
-                    if (currentNUmber <= 10) {
-                        if (j > 0) {
-                            if (secondLayer[i + 1][j] != secondLayer[i + 1][j - 1]) {
-                                System.out.print("   ");
-                            }
-                        }
-                        System.out.print("*-");
-                    } else {
-                        if (j > 0) {
-                            if (secondLayer[i + 1][j] != secondLayer[i + 1][j - 1]) {
-                                System.out.print("   ");
-                            }
-                        }
-                        System.out.print("*- ");
-                    }
+                    System.out.print("*-    ");//
                 }
                 System.out.println();
             }
             for (int j = 0; j < secondLayer[i].length; j++) {
-                if(j == 0){
+                if (j == 0) {
                     System.out.print("*- ");
                 }
                 if (j > 0) {
-                    if (secondLayer[i][j] != secondLayer[i][j - 1]) {
-                        System.out.print("*- ");
+                    if (secondLayer[i][j - 1] < 10) {
+                        if (secondLayer[i][j] != secondLayer[i][j - 1]) {
+                            System.out.print(" *-  ");
+                        } else {
+                            System.out.print("     ");
+                        }
+                    } else if (secondLayer[i][j - 1] < 100) {
+                        if (secondLayer[i][j] != secondLayer[i][j - 1]) {
+                            System.out.print(" *- ");
+                        } else {
+                            System.out.print("    ");
+                        }
+                    } else if (secondLayer[i][j - 1] < 1000) {
+                        if (secondLayer[i][j] != secondLayer[i][j - 1]) {
+                            System.out.print("*- ");
+                        } else {
+                            System.out.print("   ");
+                        }
+                    } else {
+                        if (secondLayer[i][j] != secondLayer[i][j - 1]) {
+                            System.out.print("*-");
+                        } else {
+                            System.out.print("  ");
+                        }
                     }
                 }
-                System.out.print(secondLayer[i][j] + " ");
-                if(j == secondLayer[i].length - 1){
-                    System.out.print("*-");
+                System.out.print(secondLayer[i][j]);
+                if (j == secondLayer[i].length - 1) {
+                    System.out.print(" *-");
                 }
             }
             System.out.println();
             i++;
-            if(i - 2 == 0 && i == secondLayer.length){
-                for (int j = 0; j < secondLayer[1].length; j++) {
-                    int currentNUmber = secondLayer[1][j];
+            if (i == secondLayer.length) {
+                for (int j = 0; j < secondLayer[i - 1].length; j++) {
                     if (j == 0) {
                         System.out.print("   ");
                     }
-                    if (currentNUmber < 10) {
-                        if (j > 0) {
-                            if (secondLayer[1][j] != secondLayer[1][j - 1]) {
-                                System.out.print("   ");
-                            }
-                        }
-                        System.out.print("*-");
+                    System.out.print("*-    ");
+                }
+            } else {
+                for (int j = 0; j < secondLayer[i].length; j++) {
+                    if (j == 0) {
+                        System.out.print("   ");
+                    }
+                    if (secondLayer[i][j] == secondLayer[i - 1][j]) {
+                        System.out.print("      ");
                     } else {
-                        if (j > 0) {
-                            if (secondLayer[1][j] != secondLayer[1][j - 1]) {
-                                System.out.print("   ");
-                            }
-                        }
-                        System.out.print("*- ");
+                        System.out.print("*-    ");
                     }
                 }
-            }else {
-                if (i == secondLayer.length) {
-                    for (int j = 0; j < secondLayer[i - 2].length; j++) {
-                        int currentNUmber = secondLayer[i - 2][j];
-                        if (j == 0) {
-                            System.out.print("   ");
-                        }
-                        if (currentNUmber < 10) {
-                            if (j > 0) {
-                                if (secondLayer[i - 2][j] != secondLayer[i - 2][j - 1]) {
-                                    System.out.print("   ");
-                                }
-                            }
-                            if (secondLayer[i - 2][j] == secondLayer[i - 3][j]) {
-                                System.out.print("  ");
-                            } else {
-                                System.out.print("*-");
-                            }
-                        } else {
-                            if (j > 0) {
-                                if (secondLayer[i - 2][j] != secondLayer[i - 2][j - 1]) {
-                                    System.out.print("   ");
-                                }
-                            }
-                            if (secondLayer[i - 2][j] == secondLayer[i - 3][j]) {
-                                System.out.print("   ");
-                            } else {
-                                System.out.print("*- ");
-                            }
-                        }
-                    }
-                } else {
-                    for (int j = 0; j < secondLayer[i].length; j++) {
-                        int currentNUmber = secondLayer[i][j];
-                        if (j == 0) {
-                            System.out.print("   ");
-                        }
-                        if (currentNUmber < 10) {
-                            if (j > 0) {
-                                if (secondLayer[i][j] != secondLayer[i][j - 1]) {
-                                    System.out.print("   ");
-                                }
-                            }
-                            if (secondLayer[i][j] == secondLayer[i - 1][j]) {
-                                System.out.print("  ");
-                            } else {
-                                System.out.print("*-");
-                            }
-                        } else {
-                            if (j > 0) {
-                                if (secondLayer[i][j] != secondLayer[i][j - 1]) {
-                                    System.out.print("   ");
-                                }
-                            }
-                            if (secondLayer[i][j] == secondLayer[i - 1][j]) {
-                                System.out.print("   ");
-                            } else {
-                                System.out.print("*- ");
-                            }
-                        }
-                    }
-                    System.out.println();
-                }
+                System.out.println();
             }
         }
     }
@@ -218,10 +158,8 @@ public class Brickwork {
     private static boolean addToFirstLayer(Scanner scanner, int[][] firstLayer, int length) {
         boolean wrongInput = false;
         for (int i = 0; i < firstLayer.length; i++) {
-
             String[] currentInputLine = scanner.nextLine().split("\\s+");
-            if(currentInputLine.length != length){
-
+            if (currentInputLine.length != length) {
                 wrongInput = true;
                 System.out.println("-1");
                 System.out.println("Wrong input!");
